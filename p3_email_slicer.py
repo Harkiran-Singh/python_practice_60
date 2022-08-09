aa16d8c3a1aa3233d0445baf5bbd391fd87a5144
Harkiran-Singh <harkiran.se@gmail.com>
2022-08-09 16:50:35 -0400

    added email_slicer function

--- p3_email_slicer.py
+++ p3_email_slicer.py
@@ -7,13 +7,23 @@ Created on Sun Aug  7 17:08:46 2022
 
 #harkiran.singh@hotmail.com
 
-email = str(input('Enter the email\n'))
+email = str(input('Enter the email \n'))
 """email_elements = email.split('@')
 print(email_elements)
 username = email_elements[0]
 domain = email_elements[1]
 print(f'username is {username} and domain is {domain}')"""
 
-username = email[:email.index('@')]
-domain = email[email.index('@')+1:]
-print(f'username is {username} and domain is {domain}')
+
+def email_slicer(email):
+    no_of_attherate = []
+    for x in email:
+        if x == '@':
+            no_of_attherate.extend(x)
+    if len(no_of_attherate) != 1:
+        print('invalid email')
+    else:
+        username = email[:email.index('@')]
+        domain = email[email.index('@')+1:]
+        print(f'username is {username} and domain is {domain}')
+email_slicer(email)
